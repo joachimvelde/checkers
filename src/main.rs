cbe1a06f9dca91cb84645d7c031c0eec3989ac93
Joachim Velde <kjvelde@ifi.uio.no>
2024-04-23 15:26:38 +0200

    Checkerboard

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, window::WindowResolution};
 
-static WIDTH: f32 = 1000.0;
-static HEIGHT: f32 = 1000.0;
+static WIDTH: f32 = 800.0;
+static HEIGHT: f32 = 800.0;
 static TILES: i32 = 8;
 
 #[derive(Component)]
@@ -20,27 +20,44 @@ enum CheckerColour {
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Checkers".to_string(),
+                resolution: WindowResolution::new(WIDTH, HEIGHT),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .run()
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>
-) {
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
     let tile_width: f32 = WIDTH / TILES as f32;
     let tile_height: f32 = HEIGHT / TILES as f32;
 
-    for x in 0..TILES {
-        for y in 0..TILES {
-            commands.spawn(MaterialMesh2dBundle {
-                mesh: meshes.add(Rectangle::default()).into(),
-                transform: Transform::from_xyz(x as f32, y as f32, 0.0),
-                material: materials.add(Color::PURPLE),
+    for y in -TILES/2..TILES/2 {
+        for x in -TILES/2..TILES/2 {
+            let x_pos: f32 = x as f32 * tile_width + tile_width / 2.0;
+            let y_pos: f32 = y as f32 * tile_height + tile_height / 2.0;
+
+            let color;
+            if (x + y) % 2 == 0 {
+                color = Color::RED;
+            } else {
+                color = Color::GREEN;
+            }
+
+            commands.spawn(SpriteBundle {
+                sprite: Sprite {
+                    color,
+                    custom_size: Some(Vec2::new(tile_width, tile_height)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(x_pos, y_pos, 0.0),
                 ..default()
             });
         }
