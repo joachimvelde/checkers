2f48f04af21b743b1422d6fe701b287bd6998350
j.velde <kjvelde@uio.no>
2024-04-23 20:34:45 +0200

    Added pieces

--- src/main.rs
+++ src/main.rs
@@ -45,25 +45,38 @@ fn create_board(commands: &mut Commands) {
     }
 }
 
+// TODO: Make tile_width and tile_height constants and make the x and y calculations macros
 fn create_pieces(commands: &mut Commands) {
+    let tile_width = SCREEN_WIDTH / 8.0;
+    let tile_height = SCREEN_WIDTH / 8.0;
+
     for row in 0..8 {
         for col in 0..8 {
-            if row < 3 && (row + col) as f32 % 2.0 == 0.0 {
-                // TODO: Calculate positions
-                spawn_piece(commands, Vec2::new(0.0, 0.0), Color::RED);
+            let x = tile_width / 2.0 - SCREEN_WIDTH / 2.0 + tile_width * col as f32;
+            let y = tile_height / 2.0 - SCREEN_HEIGHT / 2.0 + tile_height * row as f32;
+
+            if row > 4 && (row + col) as f32 % 2.0 != 0.0 {
+                spawn_piece(commands, Vec2::new(x, y), Color::RED);
+            }
+
+            if row < 3 && (row + col) as f32 % 2.0 != 0.0 {
+                spawn_piece(commands, Vec2::new(x, y), Color::PURPLE);
             }
         }
     }
 }
 
 fn spawn_piece(commands: &mut Commands, position: Vec2, color: Color) {
+    let piece_width = SCREEN_WIDTH / 16.0;
+    let piece_height = SCREEN_WIDTH / 16.0;
+
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color,
-            custom_size: Some(Vec2::new(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0)),
+            custom_size: Some(Vec2::new(piece_width, piece_height)),
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(position.x, position.y, 0.0)),
+        transform: Transform::from_xyz(position.x, position.y, 0.0),
         ..default()
     });
 }
