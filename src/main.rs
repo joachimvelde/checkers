9299a6f0207253ca98edd3acd50c2f1c620102f2
j.velde <kjvelde@uio.no>
2024-04-25 23:47:10 +0200

    Fixed hit detection! (i am stupid)

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,24 @@ enum PieceKind {
 #[derive(Component, Clone, Copy)]
 struct Piece {
     kind: PieceKind,
-    position: (f32, f32)
+    position: Vec2
 }
 
 #[derive(Resource)]
 struct Board {
-    centers: [[(f32, f32); 8]; 8],
+    centers: [[Vec2; 8]; 8],
     pieces: [[Option<Piece>; 8]; 8]
 }
 
-// Used to keep track of a piece being dragged
 #[derive(Resource)]
 struct DragState {
-    initial_position: (f32, f32),
+    initial_position: Vec2,
     piece: Option<Piece>
 }
 
 impl Piece {
-    fn new(kind: PieceKind, position: (f32, f32)) -> Piece {
-        Piece {
+    fn new(kind: PieceKind, position: Vec2) -> Self {
+        Self {
             kind,
             position
         }
@@ -57,40 +56,37 @@ impl Piece {
                 custom_size: Some(Vec2::new(PIECE_SIZE, PIECE_SIZE)),
                 ..default()
             },
-            transform: Transform::from_xyz(self.position.0, -self.position.1, 0.0),
+            transform: Transform::from_xyz(self.position.x, self.position.y, 0.0),
             ..default()
         });
     }
 }
 
 impl Board {
-    fn new() -> Board {
-        let mut centers = [[(0.0, 0.0); 8]; 8];
+    fn new() -> Self {
+        let mut centers = [[Vec2::default(); 8]; 8];
         let mut pieces = [[None; 8]; 8];
 
         for row in 0..8 {
             for col in 0..8 {
-                // Generate the center positions of all tiles for rendering later
-                let x = TILE_SIZE / 2.0 - SCREEN_SIZE / 2.0 + TILE_SIZE * col as f32;
-                let y = TILE_SIZE / 2.0 - SCREEN_SIZE / 2.0 + TILE_SIZE * row as f32;
-                centers[row][col] = (x, y);
+                let x = -SCREEN_SIZE / 2.0 + TILE_SIZE / 2.0 + TILE_SIZE * col as f32;
+                let y = -SCREEN_SIZE / 2.0 + TILE_SIZE / 2.0 + TILE_SIZE * (7 - row) as f32;
+                centers[row][col].x = x;
+                centers[row][col].y = y;
 
-                // Generate the pieces. These have independent positions for drag and drop to make sense
-                if row > 4 && (row + col) as f32 % 2.0 != 0.0 {
-                    pieces[row][col] = Some(Piece::new(PieceKind::Red, (x, y)));
-                    println!("Generated red piece at {}, {}", x, y);
-                }
                 if row < 3 && (row + col) as f32 % 2.0 != 0.0 {
-                    pieces[row][col] = Some(Piece::new(PieceKind::Black, (x, y)));
-                    println!("Generated black piece at {}, {}", x, y);
+                    pieces[row][col] = Some(Piece::new(PieceKind::Red, Vec2::new(x, y)));
+                    eprintln!("Red: {:?}", Vec2::new(x, y));
+                }
+
+                if row > 4 && (row + col) as f32 % 2.0 != 0.0 {
+                    pieces[row][col] = Some(Piece::new(PieceKind::Black, Vec2::new(x, y)));
+                    eprintln!("Black: {:?}", Vec2::new(x, y));
                 }
             }
         }
 
-        Board {
-            centers,
-            pieces
-        }
+        Board { centers, pieces }
     }
 
     fn render(&self, commands: &mut Commands) {
@@ -98,23 +94,21 @@ impl Board {
             for col in 0..8 {
                 let color;
                 if (row + col) as f32 % 2.0 == 0.0 {
-                    color = Color::BLACK;
-                } else {
                     color = Color::WHITE;
+                } else {
+                    color = Color::BLACK;
                 }
 
-                // This renders one tile
                 commands.spawn(SpriteBundle {
                     sprite: Sprite {
                         color,
                         custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
                         ..default()
                     },
-                    transform: Transform::from_xyz(self.centers[row][col].0, self.centers[row][col].1, 0.0),
+                    transform: Transform::from_xyz(self.centers[row][col].x, self.centers[row][col].y, 0.0),
                     ..default()
                 });
 
-                // This renders a piece on the tile
                 if let Some(piece) = self.pieces[row][col] {
                     piece.render(commands);
                 }
@@ -126,13 +120,12 @@ impl Board {
 impl DragState {
     fn new() -> DragState {
         DragState {
-            initial_position: (-999.99, -999.99),
+            initial_position: Vec2::default(),
             piece: None
         }
     }
 }
 
-// Making the screen the same size as the board is important for the drag and drop to work
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -148,7 +141,7 @@ fn main() {
         .add_systems(Update, (update, input_handler))
         .insert_resource(Board::new())
         .insert_resource(DragState::new())
-        .run();
+        .run()
 }
 
 fn setup(mut commands: Commands) {
@@ -161,53 +154,42 @@ fn update(mut commands: Commands, board: ResMut<Board>) {
 
 fn input_handler(
     mut commands: Commands,
-    mut mouse_button: Res<ButtonInput<MouseButton>>,
+    mut left_mouse: Res<ButtonInput<MouseButton>>,
     q_windows: Query<&Window, With<PrimaryWindow>>,
     board: ResMut<Board>,
     mut drag: ResMut<DragState>,
 )
 {
-    // If the mouse was just pressed, determine the pressed piece and set it to follow the mouse
-    if mouse_button.just_pressed(MouseButton::Left) {
-        let mut position: (f32, f32) = (0.0, 0.0);
+    if left_mouse.just_pressed(MouseButton::Left) {
         if let Some(w_position) = q_windows.single().cursor_position() {
-            println!("{}, {}", w_position.x - SCREEN_SIZE / 2.0, w_position.y - SCREEN_SIZE / 2.0);
-            position.0 = w_position.x;
-            position.1 = w_position.y;
-
-            if let Some(piece) = hit_piece(&mut commands, &board, position) {
+            if let Some(piece) = mouse_hit(&mut commands, &board, w_position) {
                 drag.piece = Some(piece);
-                drag.initial_position = piece.position;
+                drag.initial_position = piece.position; // NOTE: Maybe clone?
             }
         }
     }
-
-    // If the mouse is continually pressed down keep the piece following the mouse
-
-    // If the mouse is released and there is a piece being dragged, determine if the new position is valid and place the tile there
-    // On mouse release, if a piece is being dragged check if the mouse is in a valid position. Yes -> update piece, No -> Reset position
-    if mouse_button.just_released(MouseButton::Left) {
-
-    }
 }
 
-fn hit_piece(commands: &mut Commands, board: &ResMut<Board>, mouse_position: (f32, f32)) -> Option<Piece> {
-    let center_mouse_position = (mouse_position.0 - SCREEN_SIZE / 2.0, mouse_position.1 - SCREEN_SIZE / 2.0);
-    // Check if the mouse position was inside one of the tiles
+// NOTE: We have to center the mouse coordinates
+fn mouse_hit(commands: &mut Commands, board: &ResMut<Board>, mouse_position: Vec2) -> Option<Piece> {
+    let centered = Vec2::new(mouse_position.x - SCREEN_SIZE / 2.0,
+                             -mouse_position.y + SCREEN_SIZE / 2.0);
+
+    eprintln!("Centered mouse: {:?}", centered);
     for row in 0..8 {
         for col in 0..8 {
             if let Some(piece) = board.pieces[row][col] {
-                if distance(center_mouse_position, piece.position) <= PIECE_SIZE {
-                    println!("Hit a {:?} piece ({}, {}) at {} {}!", piece.kind, piece.position.0, piece.position.1, center_mouse_position.0, center_mouse_position.1);
+                if distance(centered, piece.position) <= PIECE_SIZE {
+                    eprintln!("Hit a {:?} piece at {:?}", piece.kind, piece.position);
                     return Some(piece);
                 }
             }
         }
     }
 
-    return None;
+    None
 }
 
-fn distance(a: (f32, f32), b: (f32, f32)) -> f32 {
-    ((b.0 - a.0) * (b.0 - a.0) + (b.1 - a.1) * (b.1 - a.1)).sqrt()
+fn distance(a: Vec2, b: Vec2) -> f32 {
+    ((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y)).sqrt()
 }
