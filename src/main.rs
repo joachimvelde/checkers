8f53be9b40dcc5df458eee223a9562a155e9f438
j.velde <kjvelde@uio.no>
2024-04-26 12:29:41 +0200

    This doesnt work, but it is less horrible

--- src/main.rs
+++ src/main.rs
@@ -8,120 +8,46 @@ const SCREEN_SIZE: f32 = 800.0;
 const TILE_SIZE: f32 = SCREEN_SIZE / 8.0;
 const PIECE_SIZE: f32 = TILE_SIZE / 2.0;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Debug)]
 enum PieceKind {
-    Red,
-    Black,
-    RedKing,
-    BlackKing
+    Regular,
+    King
 }
 
-#[derive(Component, Clone, Copy)]
+#[derive(Debug)]
+enum Player {
+    Red,
+    Black
+}
+
+#[derive(Component, Debug)]
 struct Piece {
     kind: PieceKind,
-    position: Vec2
+    owner: Player
 }
 
-#[derive(Resource)]
-struct Board {
-    centers: [[Vec2; 8]; 8],
-    pieces: [[Option<Piece>; 8]; 8]
+#[derive(Component)]
+struct Position {
+    row: usize,
+    col: usize
 }
+
+#[derive(Component)]
+struct Tile;
 
 #[derive(Resource)]
 struct DragState {
+    piece: Option<Entity>,
     initial_position: Vec2,
-    piece: Option<Piece>
-}
-
-impl Piece {
-    fn new(kind: PieceKind, position: Vec2) -> Self {
-        Self {
-            kind,
-            position
-        }
-    }
-
-    fn render(&self, commands: &mut Commands) {
-        let color;
-        match self.kind {
-            PieceKind::Red |
-            PieceKind::RedKing => color = Color::RED,
-            PieceKind::Black |
-            PieceKind::BlackKing => color = Color::INDIGO
-        }
-        commands.spawn(SpriteBundle {
-            sprite: Sprite {
-                color,
-                custom_size: Some(Vec2::new(PIECE_SIZE, PIECE_SIZE)),
-                ..default()
-            },
-            transform: Transform::from_xyz(self.position.x, self.position.y, 0.0),
-            ..default()
-        });
-    }
-}
-
-impl Board {
-    fn new() -> Self {
-        let mut centers = [[Vec2::default(); 8]; 8];
-        let mut pieces = [[None; 8]; 8];
-
-        for row in 0..8 {
-            for col in 0..8 {
-                let x = -SCREEN_SIZE / 2.0 + TILE_SIZE / 2.0 + TILE_SIZE * col as f32;
-                let y = -SCREEN_SIZE / 2.0 + TILE_SIZE / 2.0 + TILE_SIZE * (7 - row) as f32;
-                centers[row][col].x = x;
-                centers[row][col].y = y;
-
-                if row < 3 && (row + col) as f32 % 2.0 != 0.0 {
-                    pieces[row][col] = Some(Piece::new(PieceKind::Red, Vec2::new(x, y)));
-                    eprintln!("Red: {:?}", Vec2::new(x, y));
-                }
-
-                if row > 4 && (row + col) as f32 % 2.0 != 0.0 {
-                    pieces[row][col] = Some(Piece::new(PieceKind::Black, Vec2::new(x, y)));
-                    eprintln!("Black: {:?}", Vec2::new(x, y));
-                }
-            }
-        }
-
-        Board { centers, pieces }
-    }
-
-    fn render(&self, commands: &mut Commands) {
-        for row in 0..8 {
-            for col in 0..8 {
-                let color;
-                if (row + col) as f32 % 2.0 == 0.0 {
-                    color = Color::WHITE;
-                } else {
-                    color = Color::BLACK;
-                }
-
-                commands.spawn(SpriteBundle {
-                    sprite: Sprite {
-                        color,
-                        custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
-                        ..default()
-                    },
-                    transform: Transform::from_xyz(self.centers[row][col].x, self.centers[row][col].y, 0.0),
-                    ..default()
-                });
-
-                if let Some(piece) = self.pieces[row][col] {
-                    piece.render(commands);
-                }
-            }
-        }
-    }
+    offset: Vec2
 }
 
 impl DragState {
-    fn new() -> DragState {
-        DragState {
+    fn new() -> Self {
+        Self {
+            piece: None,
             initial_position: Vec2::default(),
-            piece: None
+            offset: Vec2::default()
         }
     }
 }
@@ -132,64 +58,117 @@ fn main() {
             primary_window: Some(Window {
                 title: "Checkers".to_string(),
                 resolution: WindowResolution::new(SCREEN_SIZE, SCREEN_SIZE),
-                resizable: false,
+                resizable: true,
                 ..default()
             }),
             ..default()
         }))
         .add_systems(Startup, setup)
-        .add_systems(Update, (update, input_handler))
-        .insert_resource(Board::new())
+        .add_systems(Update, update)
         .insert_resource(DragState::new())
-        .run()
+        .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-}
 
-fn update(mut commands: Commands, board: ResMut<Board>) {
-    board.render(&mut commands);
-}
-
-fn input_handler(
-    mut commands: Commands,
-    mut left_mouse: Res<ButtonInput<MouseButton>>,
-    q_windows: Query<&Window, With<PrimaryWindow>>,
-    board: ResMut<Board>,
-    mut drag: ResMut<DragState>,
-)
-{
-    if left_mouse.just_pressed(MouseButton::Left) {
-        if let Some(w_position) = q_windows.single().cursor_position() {
-            if let Some(piece) = mouse_hit(&mut commands, &board, w_position) {
-                drag.piece = Some(piece);
-                drag.initial_position = piece.position; // NOTE: Maybe clone?
-            }
-        }
-    }
-}
-
-// NOTE: We have to center the mouse coordinates
-fn mouse_hit(commands: &mut Commands, board: &ResMut<Board>, mouse_position: Vec2) -> Option<Piece> {
-    let centered = Vec2::new(mouse_position.x - SCREEN_SIZE / 2.0,
-                             -mouse_position.y + SCREEN_SIZE / 2.0);
-
-    eprintln!("Centered mouse: {:?}", centered);
     for row in 0..8 {
         for col in 0..8 {
-            if let Some(piece) = board.pieces[row][col] {
-                if distance(centered, piece.position) <= PIECE_SIZE {
-                    eprintln!("Hit a {:?} piece at {:?}", piece.kind, piece.position);
-                    return Some(piece);
-                }
+            let x = -SCREEN_SIZE / 2.0 + TILE_SIZE / 2.0 + TILE_SIZE * col as f32;
+            let y = -SCREEN_SIZE / 2.0 + TILE_SIZE / 2.0 + TILE_SIZE * (7 - row) as f32;
+
+            // Create tiles
+            let color;
+            if (row + col) as f32 % 2.0 == 0.0 {
+                color = Color::WHITE;
+            } else {
+                color = Color::BLACK;
+            }
+            commands.spawn(SpriteBundle {
+                sprite: Sprite {
+                    color,
+                    custom_size: Some(Vec2::new(TILE_SIZE, TILE_SIZE)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(x, y, 0.0),
+                ..default()
+            })
+            .insert(Tile)
+            .insert(Position{ row, col });
+
+            // Create pieces
+            if row < 3 && (row + col) as f32 % 2.0 != 0.0 {
+                commands.spawn(SpriteBundle {
+                    sprite: Sprite {
+                        color: Color::RED,
+                        custom_size: Some(Vec2::new(PIECE_SIZE, PIECE_SIZE)),
+                        ..default()
+                    },
+                    transform: Transform::from_xyz(x, y, 0.0),
+                    ..default()
+                })
+                .insert(Piece {
+                    kind: PieceKind::Regular,
+                    owner: Player::Red
+                })
+                .insert(Position { row, col });
+            }
+
+            if row > 4 && (row + col) as f32 % 2.0 != 0.0 {
+                commands.spawn(SpriteBundle {
+                    sprite: Sprite {
+                        color: Color::PURPLE,
+                        custom_size: Some(Vec2::new(PIECE_SIZE, PIECE_SIZE)),
+                        ..default()
+                    },
+                    transform: Transform::from_xyz(x, y, 0.0),
+                    ..default()
+                })
+                .insert(Piece {
+                    kind: PieceKind::Regular,
+                    owner: Player::Black
+                })
+                .insert(Position { row, col });
             }
         }
     }
-
-    None
 }
 
-fn distance(a: Vec2, b: Vec2) -> f32 {
-    ((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y)).sqrt()
+fn update
+(
+    mut commands: Commands,
+    left_mouse: Res<ButtonInput<MouseButton>>,
+    window: Query<&Window, With<PrimaryWindow>>,
+    q_camera: Query<(&Camera, &GlobalTransform)>,
+    tiles: Query<(&Tile, &Position, &Transform)>,
+    mut pieces: Query<(Entity, &Piece, &Position, &Transform)>,
+    mut drag: ResMut<DragState>
+)
+{
+    let (camera, camera_transform) = q_camera.single();
+    if let Some(mouse_position) = window.single().cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+            .map(|ray| ray.origin.truncate())
+            {
+                // Check for hits
+                if left_mouse.just_pressed(MouseButton::Left) {
+                    for (entity, piece, position, transform) in pieces.iter() {
+                        // Detect hits
+                        let center = transform.translation.truncate();
+                        if mouse_position.distance(center) <= PIECE_SIZE {
+                            eprintln!("{:?} clicked!", piece);
+                            drag.piece = Some(entity);
+                        }
+                    }
+                }
+
+                // Check for drags
+                if left_mouse.pressed(MouseButton::Left) {
+                    if let Some(entity) = drag.piece {
+                        if let Ok(transform) = pieces.get_mut(entity) {
+                            eprintln!("Dragging");
+                        }
+                    }
+                }
+            }
 }
