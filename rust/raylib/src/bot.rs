b2fea2837ce777f660ff2fd9ef5be74066051975
Joachim Velde <kjvelde@ifi.uio.no>
2024-08-04 14:24:12 +0200

    Alpha-beta pruning (big performance boost)

--- rust/raylib/src/bot.rs
+++ rust/raylib/src/bot.rs
@@ -2,40 +2,52 @@ use crate::board::{Board, Move, Player, PieceKind};
 
 use std::{thread, time};
 
-const MINIMAX_DEPTH: i32 = 5;
+const MINIMAX_DEPTH: i32 = 9;
 
 pub fn get_bot_move(board: &Board) -> Move {
     // thread::sleep(time::Duration::from_millis(500));
-
-    let (_, m) = minimax(board, MINIMAX_DEPTH);
-
+    let (_, m) = minimax(board, MINIMAX_DEPTH, -f32::INFINITY, f32::INFINITY);
     return m.unwrap();
 }
 
-pub fn minimax(board: &Board, depth: i32) -> (f32, Option<Move>) {
+pub fn minimax(board: &Board, depth: i32, alpha: f32, beta: f32) -> (f32, Option<Move>) {
     if board.is_game_over() || depth == 0 {
         return (state_value(board), None);
     }
 
+    let mut alpha = alpha;
+    let mut beta = beta;
     let mut best_move = Move::default();
 
     if board.get_turn() == Player::BLACK {
         let mut value = f32::INFINITY;
         for m in board.get_all_legal_moves(Player::BLACK) {
-            let (next_value, _) = minimax(&result(&board, m), depth - 1);
+            let (next_value, _) = minimax(&result(&board, m), depth - 1, alpha, beta);
+
             if next_value < value {
                 best_move = m;
                 value = next_value;
+            }
+
+            beta = beta.min(value);
+            if beta <= alpha {
+                break;
             }
         }
         return (value, Some(best_move));
     } else {
         let mut value = -f32::INFINITY;
         for m in board.get_all_legal_moves(Player::RED) {
-            let (next_value, _) = minimax(&result(&board, m), depth - 1);
+            let (next_value, _) = minimax(&result(&board, m), depth - 1, alpha, beta);
+
             if next_value > value {
                 best_move = m;
                 value = next_value;
+            }
+
+            alpha = alpha.max(value);
+            if alpha >= alpha {
+                break;
             }
         }
         return (value, Some(best_move));
